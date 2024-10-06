dc5e415310ff578adb30b176906b9309402e0762
emily <emilyvicki601@gmail.com>
2024-10-07 00:42:40 +0800

    chore: remove compiled js files from source control

--- src/api/config.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-// create an axios instance
-const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || 'http://127.0.0.1:8000/api', // url = base url + request url
-    headers: {
-        'Content-Type': 'application/json'
-    }
-});
-export default api;
